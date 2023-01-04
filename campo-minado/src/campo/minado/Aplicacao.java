5e784e12a059c37f7be51f9497089534b5cc2dd0
carlosaugusto28 <109244715+carlosaugusto28@users.noreply.github.com>
2023-01-04 19:48:56 -0300

    Campo Minado

---
+++ campo-minado/src/campo/minado/Aplicacao.java
@@ -0,0 +1,14 @@
+package campo.minado;
+
+import campo.minado.modelo.Tabuleiro;
+import campo.minado.visao.TabuleiroConsole;
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		
+		Tabuleiro tabuleiro = new Tabuleiro(6, 6, 3);
+		new TabuleiroConsole(tabuleiro);
+		
+	}
+}
